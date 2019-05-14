94cfb3d364d9c6908b4ca5afa95636b278863784
Biswas <BISWAPR2@novartis.net>
2019-05-15 00:16:22 +0200

    Commit from Pen drive

---
+++ HelloMVCWorld/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Models.Movie movie = new Models.Movie()
+            {
+                Title = "The Godfather",
+                ReleaseDate = new DateTime(1972, 3, 24)
+            };
+            return View(movie);
+        }
+    }
+}
